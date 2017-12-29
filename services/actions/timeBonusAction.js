02b768dc631f445ed048bde2f7c6490243d84c8e
zyevegor <zyev.egor@gmail.com>
2017-12-29 09:45:11 +0300

    https://chronobank.atlassian.net/browse/MD-73 - eth bugs and features

--- services/actions/timeBonusAction.js
+++ services/actions/timeBonusAction.js
@@ -6,31 +6,30 @@ const _ = require('lodash'),
   nemServices = require('../nemServices');
 
 const events = ['Deposit', 'WithdrawShares', 'FeatureFeeTaken'];
-const contracts = ['ERC20Interface', 'ERC20Manager']; /*eslint no-unused-vars: off */
 
-const NEM_DIVISIBILITY = _.get(config.nem, 'divisibillity', 1);
-const TIME_DIVISIBILITY = 100000000;
-
-const updateTimeBalance = (address, balance) =>
-  accountModel.findOneAndUpdate({address: address}, {$set: {maxTimeBalance: balance}}, {upsert: true});
-
-async function run () {
-  const recipient = _.get(this.event, 'payload.who');
-  log.info(`TimeBonus run for ${this.address}`);
+async function run (event, contracts) {
+  const recipient = _.get(event, 'payload.who');
+  log.info(`TimeBonus run for ${recipient}`);
   // Obtain required settings from network
-  const erc = await this.contracts.ERC20Manager.deployed(), // Get the instance of contract
+  let erc = await contracts.ERC20Manager.deployed(), // Get the instance of contract
     token = await erc.getTokenAddressBySymbol('TIME'), // Get address of token by symbol
-    time = await this.contracts.ERC20Interface.at(token), // get instance of token
-    balance = await time.balanceOf(recipient), // obtain balance of time for recipient BigNumber
-    user = (await accountModel.findOne({ address: recipient })).toObject(), // load recipient's record from DB
-    maxTimeBalance = _.get(user, 'maxTimeBalance', 0), // get maxTimeBalance from record
-    bonusRate = _.get(config.nem, 'timeBonus.rate', 0),
+    time = await contracts.ERC20Interface.at(token), // get instance of token
+    balance = await time.balanceOf(recipient); // obtain balance of time for recipient BigNumber
+
+  let user = await accountModel.findOne({address: recipient}); // load recipient's record from DB
+
+  if (!user)
+    return;
+
+  user = user.toObject();
+
+  let maxTimeBalance = _.get(user, 'maxTimeBalance', 0), // get maxTimeBalance from record
     nemAddress = _.get(user, 'nem'); // get NEM address from record
 
-  if(balance.greaterThan(maxTimeBalance) && nemAddress) {
-    const transferAmount = Math.round(balance.minus(maxTimeBalance).valueOf() / TIME_DIVISIBILITY) * bonusRate * NEM_DIVISIBILITY;
+  if (balance.greaterThan(maxTimeBalance) && nemAddress) {
+    const transferAmount = Math.round(balance.minus(maxTimeBalance).valueOf() / config.nem.timeDivisibility) * config.nem.timeBonus.rate * config.nem.divisibillity;
     log.info('transferAmount: ', transferAmount, balance.minus(maxTimeBalance).valueOf());
-    await updateTimeBalance(recipient, balance.toNumber());
+    await accountModel.findOneAndUpdate({address: recipient}, {$set: {maxTimeBalance: balance.toNumber()}});
     return await nemServices.makeBonusTransfer(nemAddress, transferAmount);
   }
 }
--- services/actions/welcomeBonusAction.js
+++ services/actions/welcomeBonusAction.js
@@ -6,21 +6,24 @@ const _ = require('lodash'),
   log = bunyan.createLogger({name: 'nemActionProcessor.welcomeBonusAction'});
 
 const events = ['SetHash'];
-const contracts = ['UserManager']; /*eslint no-unused-vars: off */
-const NEM_DIVISIBILITY = _.get(config.nem, 'divisibillity', 1);
 
-async function run () {
-  const recipient = _.get(this.event, 'payload.key');
+async function run (event) {
+  const recipient = _.get(event, 'payload.key');
   log.info(`Running welcomeBonus ${recipient}`);
-  
-  const user = (await accountModel.findOne({ address: recipient })).toObject(), // load recipient's record from DB
-    nemAddress = _.get(user, 'nem'), // get NEM address from record
-    isWBSent = _.get(user, 'welcomeBonusSent', false),
-    amount = _.get(config.nem, 'welcomeBonus.amount', 1);
-  
-  if(nemAddress && !isWBSent) {
-    await accountModel.findOneAndUpdate({address: recipient}, {$set: {welcomeBonusSent: true}}, {upsert: true});
-    return await nemServices.makeBonusTransfer(nemAddress, amount * NEM_DIVISIBILITY);    
+
+  let user = await accountModel.findOne({address: recipient}); // load recipient's record from DB
+
+  if (!user)
+    return;
+
+  user = user.toObject();
+
+  const nemAddress = _.get(user, 'nem'), // get NEM address from record
+    isWBSent = _.get(user, 'welcomeBonusSent', false);
+
+  if (nemAddress && !isWBSent) {
+    await accountModel.findOneAndUpdate({address: recipient}, {$set: {welcomeBonusSent: true}});
+    return await nemServices.makeBonusTransfer(nemAddress, config.nem.welcomeBonus.amount * config.nem.divisibillity);
   }
 }
 
