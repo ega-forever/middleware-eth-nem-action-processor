384f946fc16f21a52876bb60256765e50bd00c63
Punito <maximfordevelop@gmail.com>
2018-04-28 22:42:31 +0300

    MD-187

--- tests/index.js
+++ tests/index.js
@@ -22,13 +22,14 @@ const config = require('../config'),
 mongoose.Promise = Promise;
 mongoose.accounts = mongoose.createConnection(config.mongo.accounts.uri);
 mongoose.connect(config.mongo.data.uri, {useMongoClient: true});
+
+const web3 = new Web3();
+const provider = new Web3.providers.IpcProvider(config.web3.uri, net);
 let accounts;
 
 describe('core/nem processor', function () {
 
     before(async () => {
-        const web3 = new Web3();
-        const provider = new Web3.providers.IpcProvider(config.web3.uri, net);
         web3.setProvider(provider);
         accounts = await Promise.promisify(web3.eth.getAccounts)();
         await generateEvents(accounts[0], provider);
@@ -36,8 +37,9 @@ describe('core/nem processor', function () {
     });
 
     after(async () => {
-      await deleteModels();
-      await mongoose.connection.close();
+        await deleteModels();
+        await mongoose.disconnect();
+        await web3.currentProvider.connection.end();
     });
 
     it('test aggregate data base', async () => {
@@ -49,39 +51,39 @@ describe('core/nem processor', function () {
     });
 
     it('test welcome bonus action', async () => {
-      await Promise.delay(1000);
-      let oldBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
-      let result =  await welcomeBonus(accounts[0], valueConfig.amount, valueConfig.nem_address);
+        await Promise.delay(1000);
+        let oldBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
+        let result =  await welcomeBonus(accounts[0], valueConfig.amount, valueConfig.nem_address);
 
-      await Promise.delay(60000);
-      let newBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
+        await Promise.delay(60000);
+        let newBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
 
-      expect(result.code).to.be.equal(1);
-      expect(newBalance - oldBalance).to.be.equal(100);
+        expect(result.code).to.be.equal(1);
+        expect(newBalance - oldBalance).to.be.equal(100);
     });
 
     it('test time bonus action', async () => {
-      await Promise.delay(1000);
-      let oldBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
-      let result = await timeBonus(accounts[0], valueConfig.currentAmount, valueConfig.depositMaxAmount, valueConfig.nem_address);
+        await Promise.delay(1000);
+        let oldBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
+        let result = await timeBonus(accounts[0], valueConfig.currentAmount, valueConfig.depositMaxAmount, valueConfig.nem_address);
 
-      await Promise.delay(60000);
-      let newBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
+        await Promise.delay(60000);
+        let newBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
 
-      expect(result.code).to.be.equal(1);
-      expect(newBalance - oldBalance).to.be.equal(120);
+        expect(result.code).to.be.equal(1);
+        expect(newBalance - oldBalance).to.be.equal(120);
     });
 
     it('test xem bonus action', async () => {
-      await Promise.delay(1000);
-      let xemBalance = await checkBalanceUser.nemBalance(valueConfig.nem_address);
-      let oldBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
-      let result = await xemBonus(valueConfig.nem_address, valueConfig.maxXemAmount, accounts[0]);
+        await Promise.delay(1000);
+        let xemBalance = await checkBalanceUser.nemBalance(valueConfig.nem_address);
+        let oldBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
+        let result = await xemBonus(valueConfig.nem_address, valueConfig.maxXemAmount, accounts[0]);
 
-      await Promise.delay(60000);
-      let newBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
+        await Promise.delay(60000);
+        let newBalance = await checkBalanceUser.mosaicBalance(valueConfig.nem_address);
 
-      expect(result.code).to.be.equal(1);
-      expect(newBalance - oldBalance).to.be.equal(xemBalance);
+        expect(result.code).to.be.equal(1);
+        expect(newBalance - oldBalance).to.be.equal(xemBalance);
     });
 });
