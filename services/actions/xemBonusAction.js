c22966a51138776d6e9f80dc70a5babb9bd7e045
egor <zyev.egor@rambler.ru>
2018-06-06 13:29:54 +0300

    *bonus fix commit

--- services/actions/xemBonusAction.js
+++ services/actions/xemBonusAction.js
@@ -5,28 +5,28 @@
  */
 
 const bunyan = require('bunyan'),
-  log = bunyan.createLogger({name: 'nemActionProcessor.xemBonusAction'}),
-  config = require('../../config'),
-  _ = require('lodash'),
-  accountModel = require('../../models/accountModel'),
-  nemServices = require('../nemServices');
+    log = bunyan.createLogger({name: 'nemActionProcessor.xemBonusAction'}),
+    config = require('../../config'),
+    _ = require('lodash'),
+    accountModel = require('../../models/accountModel'),
+    nemServices = require('../nemServices');
 
 module.exports = async (nemAddress, maxXemAmount) => {
-  log.info(`XemBonus run for ${nemAddress}`);
+    log.info(`XemBonus run for ${nemAddress}`);
 
-  const balances = await nemServices.getHistoricalBalance(nemAddress, config.nem.xemBonus.window.start, config.nem.xemBonus.window.end);
+    const balances = await nemServices.getHistoricalBalance(nemAddress, config.nem.xemBonus.window.start, config.nem.xemBonus.window.end);
 
-  const maxBalance = _.chain(balances)
-    .orderBy('balance')
-    .head()
-    .get('balance', 0)
-    .value();
+    const maxBalance = _.chain(balances)
+        .orderBy('balance')
+        .head()
+        .get('balance', 0)
+        .value();
 
-  const transferAmount = (maxBalance - maxXemAmount) / (config.nem.xemBonus.xemDivisibility * config.nem.xemBonus.rate) * config.nem.divisibillity;
-  if (transferAmount >= 1) {
-    const result = await nemServices.makeBonusTransfer(nemAddress, transferAmount, 'Xem Bonus');
-    await accountModel.updateMany({nem: nemAddress}, {$set: {maxXemAmount: maxBalance}, $inc: {transferLimit: 1}});
-    return result;
-  }
-  return 1;
+    const transferAmount = (maxBalance - (maxBalance * (20 / 200))) / (config.nem.xemBonus.xemDivisibility * 20) * config.nem.divisibillity;
+    if (transferAmount >= 1) {
+        const result = await nemServices.makeBonusTransfer(nemAddress, transferAmount, 'Xem Bonus');
+        await accountModel.updateMany({nem: nemAddress}, {$set: {maxXemAmount: maxBalance}, $inc: {transferLimit: 1}});
+        return result;
+    }
+    return 1;
 };
